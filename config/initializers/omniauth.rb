1ab0664125f8167e73fa07a3fa2ca1a20ee7f6fa
Justin Cook <jcook@aea267.k12.ia.us>
2013-07-14 10:47:31 -0500

    Changed Google provider variables.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['OMNIAUTH_PROVIDER_KEY'], ENV['OMNIAUTH_PROVIDER_SECRET']
+  provider :google_oauth2, ENV['OMNIAUTH_GOOGLE_PROVIDER_KEY'], ENV['OMNIAUTH_GOOGLE_PROVIDER_SECRET']
 end
